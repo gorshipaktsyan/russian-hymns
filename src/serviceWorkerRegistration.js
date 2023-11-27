5e0407357610ebd649e24603830651652e013918
Gor <gor.shipaktsyan@groundtruth.com>
2023-11-27 21:33:15 +0400

    fix

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -11,7 +11,6 @@
 // opt-in, read https://cra.link/PWA
 
 // Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
@@ -52,18 +51,6 @@ export function register(config) {
         registerValidSW(swUrl, config);
       }
     });
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent the mini-infobar from appearing on mobile
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI notify the user they can install the PWA
-      // showInstallPromotion();
-      // Optionally, send analytics event that PWA install promo was shown.
-      console.log(`'beforeinstallprompt' event was fired.`);
-    });
-
   }
 }
 
