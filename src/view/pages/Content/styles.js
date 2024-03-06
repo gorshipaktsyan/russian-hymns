c59fc7279eadfe1b227914c21ced89834130cc22
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2024-03-06 20:38:28 +0400

    fix

--- src/view/pages/Content/styles.js
+++ src/view/pages/Content/styles.js
@@ -5,16 +5,12 @@ const StyledContentComponents = {
   StyledSubList: styled(Box)({
     maxWidth: "630px",
     marginLeft: "20px",
-    marginTop: "30px",
     marginBottom: "30px",
-    // backgroundColor: "#FDFDF1",
   }),
   StyledSubHymnsList: styled(Box)({
     maxWidth: "610px",
-    marginTop: "20px",
     marginBottom: "20px",
     marginLeft: "20px",
-    // backgroundColor: "#FDFDF4",
   }),
 };
 
